abb4217b115674cc17d321056c79b2b65d69d9b8
Jon Gold <hello@designedbygold.com>
2012-11-25 18:52:04 +0000

    Initial Commit

---
+++ Hypnosister/JGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JGAppDelegate.h
+//  Hypnosister
+//
+//  Created by Jon Gold on 25/11/2012.
+//  Copyright (c) 2012 Jon Gold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Jon Gold on 25/11/2012.
+//  Copyright (c) 2012 Jon Gold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JGAppDelegate class]));
+    }
+}
